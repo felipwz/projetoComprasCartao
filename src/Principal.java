eaa8c8ace828a8e020ebdba557f0f3585a1058eb
lfsouza <luizfelipesiqueira83@gmail.com>
2024-11-17 17:54:42 -0300

    Validação de saldo

--- src/Principal.java
+++ src/Principal.java
@@ -13,34 +13,43 @@ public class Principal {
         double limite = leitura.nextDouble();
         Conta cartao = new Conta(limite);
 
-        int sair = 1;
 
-        while (sair != 0){
-            System.out.println("Digite a descrição da sua compra");
+        int sair = 1;
+        while (sair != 0) {
+
+            if (cartao.getSaldo() == 0) {
+                System.out.println("/----/10");
+                System.out.println("Você está sem saldo");
+                break;
+            }
+            System.out.println("-----");
+            System.out.println("Digite a descrição da sua compra:");
             String descricao = leitura.next();
 
-            System.out.println("Digite o valor de sua compra");
+            System.out.println("Digite o valor de sua compra:");
             double valor = leitura.nextDouble();
 
             Compras compra = new Compras(descricao, valor);
             boolean compraRealizada = cartao.lancaCompra(compra);
 
-            if (compraRealizada){
+            if (compraRealizada) {
                 System.out.println("Compra realizada!");
                 System.out.println("Seu saldo atual é de: R$" + cartao.getSaldo());
                 System.out.println("Para continuar digite 1, para sair digite 0");
                 sair = leitura.nextInt();
-            }else if (cartao.getSaldo() == 0){
-                System.out.println("Você está sem saldo!");
-                sair = 0;
 
             } else {
+
                 System.out.println("Seu saldo é insuficiente.. " +
                         "Você tem: R$" + cartao.getSaldo() +
                         " Em sua conta.");
                 System.out.printf("---");
+
+
                 System.out.printf("Se deseja tentar comprar outro produto, digite 1 ou 0 para sair");
                 sair = leitura.nextInt();
+
+
             }
 
             System.out.println("*---------------*");
@@ -51,17 +60,18 @@ public class Principal {
             Collections.sort(cartao.getCompras());
 
 
-            for (Compras c : cartao.getCompras()){
-                System.out.println(c.getDescricacao() + " - " + "R$" + c.getValor() );
+            for (Compras c : cartao.getCompras()) {
+                System.out.println(c.getDescricacao() + " - " + "R$" + c.getValor());
+
 
             }
 
 
-
-
-
-
         }
+        System.out.println("Sistema finalizado");
 
     }
+
 }
+
+
